aff98ef31a9669e037a5c969c42d466055160c73
MaxPlayer2011 <maximus.naumus@gmail.com>
2022-02-04 18:23:21 -0500

    Update nuke.js

--- commands/nuke.js
+++ commands/nuke.js
@@ -3,5 +3,5 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('nuke')
-        .setDescription('**NUKE EVERYTHING!!! HARHARHAR!!!**')
+        .setDescription('NUKE EVERYTHING!!! HARHARHAR!!!')
 }
