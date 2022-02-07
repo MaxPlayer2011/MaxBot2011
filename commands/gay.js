563d3e8e2941b5f0858c650231dd4715cfe7e190
MaxPlayer2011 <maximus.naumus@gmail.com>
2022-02-07 17:01:56 -0500

    Update gay.js

--- commands/gay.js
+++ commands/gay.js
@@ -4,5 +4,8 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('gay')
         .setDescription('are u gay?')
-        .addUserOption(option => option.setName('target').setDescription('Select a user'))
+        .addUserOption(option =>
+            option
+                .setName('target')
+                .setDescription('Select a user'))
 }
